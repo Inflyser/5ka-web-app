dbdfd2b8ff527638847b0bad04339e7ca63c5b0e
Inflyser <89538138944s@gmail.com>
2025-06-24 18:35:28 +0300

    front

--- scripts/map.js
+++ scripts/map.js
@@ -17,27 +17,30 @@ function searchAddress() {
 
 
 ymaps.ready(function () {
+    // Создаём карту
     map = new ymaps.Map("map", {
         center: [55.751574, 37.573856],
         zoom: 16,
         controls: ['zoomControl', 'geolocationControl']
     });
 
+    // Маркер в центре карты
     centerMarker = new ymaps.Placemark(map.getCenter(), {}, {
         preset: 'islands#redIcon',
         iconColor: '#ff0000'
     });
     map.geoObjects.add(centerMarker);
 
+    // Отслеживание движения карты
     map.events.add('boundschange', function () {
-        const coords = map.getCenter();
+        const coords = map.getCenter(); // [lat, lon]
+        const lat = coords[0];
+        const lon = coords[1];
+        lastCoords = coords;
+
         centerMarker.geometry.setCoordinates(coords);
 
-        ymaps.geocode(coords).then(function (res) {
-            const firstGeoObject = res.geoObjects.get(0);
-            const address = firstGeoObject ? firstGeoObject.getAddressLine() : 'не найден';
-            document.getElementById('address').innerText = 'Адрес: ' + address;
-            document.getElementById('address-input').value = address;
-        });
+        if (debounceTimeout) clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => preloadStore(lat, lon), 1000);
     });
 });
