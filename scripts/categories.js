d5057e7e74b059f5686a76a7e09fba60b1c156b0
Inflyser <89538138944s@gmail.com>
2025-06-27 15:16:37 +0300

    front

--- scripts/categories.js
+++ scripts/categories.js
@@ -16,22 +16,25 @@ function renderCategories(rawCategories) {
         listElem.appendChild(parentLi);
 
         const subUl = document.createElement('ul');
+        subUl.classList.add('subcategory-grid');
 
         parent.categories.forEach(sub => {
             const subLi = document.createElement('li');
             subLi.innerHTML = `
-                <span>${sub.name}</span><br>
-                <img src="${sub.image_link}" alt="${sub.name}" width="50"><br>
-                <small style="color:${sub.title_color}">ID: ${sub.id}</small>
+                <div class="subcategory-card">
+                    <img src="${sub.image_link}" alt="${sub.name}" class="subcategory-image">
+                    <div class="subcategory-info">
+                        <span class="subcategory-name">${sub.name}</span>
+                        <small class="subcategory-id" style="color:${sub.title_color}">ID: ${sub.id}</small>
+                    </div>
+                </div>
             `;
-
             subUl.appendChild(subLi);
         });
 
         listElem.appendChild(subUl);
     });
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
     const rawCategories = JSON.parse(localStorage.getItem('categories'));
