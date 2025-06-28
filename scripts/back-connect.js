eba9210a4f370c655799d987245903db306fde4b
Inflyser <89538138944s@gmail.com>
2025-06-28 17:34:59 +0300

    fixed bags

--- scripts/back-connect.js
+++ scripts/back-connect.js
@@ -64,20 +64,20 @@ if (checkDeliveryBtn) {
 
 
 
-// Отдельная функция для получения товаров по категории и магазину
-export async function getProducts(store_id, category_id) {
-    const response = await fetch("https://fiveka-web-app.onrender.com/get-products", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ store_id, category_id }),
-    });
+// // Отдельная функция для получения товаров по категории и магазину
+// export async function getProducts(store_id, category_id) {
+//     const response = await fetch("https://fiveka-web-app.onrender.com/get-products", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ store_id, category_id }),
+//     });
 
-    if (!response.ok) {
-        const error = await response.json();
-        throw new Error(error.detail || "Ошибка при получении товаров");
-    }
+//     if (!response.ok) {
+//         const error = await response.json();
+//         throw new Error(error.detail || "Ошибка при получении товаров");
+//     }
 
-    return await response.json();
-}
+//     return await response.json();
+// }
