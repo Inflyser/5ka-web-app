4851cd9c79227c7957fac60d156216523615eb4b
Inflyser <89538138944s@gmail.com>
2025-06-29 00:17:49 +0300

    fixed

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def webapp_builder() -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     builder.button(
         text="Click",
-        web_app=WebAppInfo(url=f"{FRONTEND_URL}/index.html")
+        web_app=WebAppInfo(url=f"{FRONTEND_URL}/page/catalog.html")
     )
     return builder.as_markup()
 
