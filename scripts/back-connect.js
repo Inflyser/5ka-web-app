3c1589de239c66beef668961e7a46f4ac29e8253
Inflyser <89538138944s@gmail.com>
2025-07-01 17:12:39 +0300

    - сom

--- scripts/back-connect.js
+++ scripts/back-connect.js
@@ -58,25 +58,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 console.log("✅ Скрипт back-connect.js загружен");
-
-
-
-
-
-// // Отдельная функция для получения товаров по категории и магазину
-// export async function getProducts(store_id, category_id) {
-//     const response = await fetch("https://fiveka-web-app.onrender.com/get-products", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ store_id, category_id }),
-//     });
-
-//     if (!response.ok) {
-//         const error = await response.json();
-//         throw new Error(error.detail || "Ошибка при получении товаров");
-//     }
-
-//     return await response.json();
-// }
